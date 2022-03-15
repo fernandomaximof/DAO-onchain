eb62e863258412d949e4a528f7b47095f635a8ce
Fernando Maximo Ferreira <fernandomaximof@Fernandos-MBP.hub>
2022-03-15 19:55:48 +1300

    updated deployer

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,13 @@
-const { MIN_DELAY, VOTING_PERIOD, VOTING_DELAY, QUORUM_PERCENTAGE } = require('./helper_hardhat_config');
+const { ethers } = require('hardhat');
+const { MIN_DELAY, VOTING_PERIOD, VOTING_DELAY, QUORUM_PERCENTAGE, ADDRESS_ZERO } = require('./helper_hardhat_config');
 
 async function main() {
+    const [ deployer ] = await ethers.getSigners();
+
     const GovernanceToken = await ethers.getContractFactory("GovernanceToken");
     const TimeLock = await ethers.getContractFactory("TimeLock");
     const GovernorContract = await ethers.getContractFactory("GovernorContract");
+    const Box = await ethers.getContractFactory("Box");
     
     const governanceToken = await GovernanceToken.deploy();
     const timeLock = await TimeLock.deploy(MIN_DELAY, [], []);
@@ -12,13 +16,29 @@ async function main() {
         QUORUM_PERCENTAGE, 
         VOTING_PERIOD, 
         VOTING_DELAY);
+    const box = await Box.deploy();
 
     console.log("GovernanceToken to:", governanceToken.address);
     console.log("TimeLock to:", timeLock.address);
     console.log("GovernorContract to:", governorContract.address);
+    console.log("Box to:", box.address);
 
-    const [ deployer ] = await ethers.getSigners();
+    const proposerRole = timeLock.PROPOSER_ROLE();
+    const executorRole = timeLock.EXECUTOR_ROLE();
+    const adminRole = timeLock.TIMELOCK_ADMIN_ROLE();
+    const proposerTx = await timeLock.grantRole(proposerRole, governorContract.address);
+    await proposerTx.wait(1);
+    const executorTx = await timeLock.grantRole(executorRole, ADDRESS_ZERO);
+    await executorTx.wait(1);
+    const revokeTx = await timeLock.revokeRole(adminRole, deployer.address);
+    await revokeTx.wait(1);
+
+    const transferBoxOnwerTx = await box.transferOwnership(timeLock.address);
+    await transferBoxOnwerTx.wait(1);
+
     await governanceToken.delegate(deployer.address);
+    const balance = BigInt(await governanceToken.balanceOf(deployer.address));
+    console.log("Deployer balance :", balance);
 }
   
   main()
